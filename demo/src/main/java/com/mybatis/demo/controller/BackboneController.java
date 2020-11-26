01af443ea3214ba8a40d21ba02a0aec6d72131b1
Kapil Chauhan <kapil.chauhan@tothenew.com>
2020-11-26 14:20:45 +0530

    modified some naming conventions

--- demo/src/main/java/com/mybatis/demo/controller/BackboneController.java
+++ demo/src/main/java/com/mybatis/demo/controller/BackboneController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BackboneController {
 
-    @GetMapping("/getBackbone")
+    @GetMapping("/home")
     public String getBackbonePage(){
         return "backbone";
     }
